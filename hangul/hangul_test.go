de06853d185493f43dfef66130a595eaf30fb008
dh.park <dh.park@navercorp.com>
2016-06-29 00:45:27 +0900

    add hangul

---
+++ hangul/hangul_test.go
@@ -0,0 +1,9 @@
+package hangul
+
+import "fmt"
+
+func ExampleHasConsonantSuffix() {
+	fmt.Println(HasConsonantSuffix("Go 언어"))
+	fmt.Println(HasConsonantSuffix("그럼"))
+	fmt.Println(HasConsonantSuffix("우리 밥 먹고 합시다."))
+}
